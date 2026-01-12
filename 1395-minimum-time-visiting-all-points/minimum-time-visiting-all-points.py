c42037b62fb4784955cb626ee7a130911055d761
Sree Lekshmi Prasannan <72271005+Sree-lekshmi99@users.noreply.github.com>
2026-01-12 15:52:31 -0800

    Time: 0 ms (100.00%) | Memory: 19.4 MB (5.66%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
@@ -3,11 +3,9 @@ class Solution:
         ans = 0
         i = 0
         while i <len(points)-1:
-            x1 =  abs(points[i][0] - points[i+1][0])
-            x2 =  abs(points[i][1] - points[i+1][1])
+            ans+=  max(abs(points[i][0] - points[i+1][0]), abs(points[i][1] - points[i+1][1]))
             i+=1
-            m = max(x1,x2)
-            ans +=m
+            
         return ans
 
         

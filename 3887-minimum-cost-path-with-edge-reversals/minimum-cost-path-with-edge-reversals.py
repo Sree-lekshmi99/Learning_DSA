ce33b59458019210b295b2ae937bc19e28c49c08
Sree Lekshmi Prasannan <72271005+Sree-lekshmi99@users.noreply.github.com>
2026-01-27 11:59:51 -0800

    Time: 415 ms (97.60%) | Memory: 70.8 MB (74.52%) - LeetSync

--- 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.py
+++ 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.py
@@ -1,12 +1,9 @@
 class Solution:
     def minCost(self, n: int, edges: List[List[int]]) -> int:
-        # Building augmented graph
         adj = defaultdict(list)
         for u, v, w in edges:
             adj[u].append((v, w))
-            adj[v].append((u, 2 * w))  # Reverse edge
-
-        # Initialize distance array from 0 node
+            adj[v].append((u, 2 * w))  
         dist = [math.inf] * n
         dist[0] = 0
 
@@ -14,12 +11,12 @@ class Solution:
         heap = [(0, 0)]
         while heap:
             d, u = heapq.heappop(heap)
-            if u == n - 1:  # Early exit
+            if u == n - 1:  
                 return d
-            if d != dist[u]:  # Stale edge
+            if d != dist[u]:  
                 continue
             for v, w in adj[u]:
-                if dist[u] + w < dist[v]:  # Edge relaxation
+                if dist[u] + w < dist[v]:  
                     dist[v] = dist[u] + w
                     heapq.heappush(heap, (dist[v], v))
 

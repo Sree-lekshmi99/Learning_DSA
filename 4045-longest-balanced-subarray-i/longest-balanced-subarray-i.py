f41f01e9779875f32289a31a4cf077d8cc03ba64
Sree Lekshmi Prasannan <72271005+Sree-lekshmi99@users.noreply.github.com>
2026-02-10 11:35:23 -0800

    Time: 1793 ms (9.13%) | Memory: 19.7 MB (13.28%) - LeetSync

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.py
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.py
@@ -1,30 +1,18 @@
 class Solution:
     def longestBalanced(self, nums: List[int]) -> int:
-        even_id = {}
-        odd_id = {}
-        for x in nums:
-            if x % 2 == 0:
-                if x not in even_id:
-                    even_id[x] = len(even_id)
-            else:
-                if x not in odd_id:
-                    odd_id[x] = len(odd_id)
+        max_len = 0
 
-        ans = 0
-        n = len(nums)
+        for i in range(len(nums)):
+            odd = {}
+            even = {}
 
-        for i in range(n):
-            even_mask = 0
-            odd_mask = 0
-            for j in range(i, n):
-                x = nums[j]
-                if x % 2 == 0:
-                    even_mask |= 1 << even_id[x]
+            for j in range(i, len(nums)):
+                if nums[j] & 1:
+                    odd[nums[j]] = odd.get(nums[j], 0) + 1
                 else:
-                    odd_mask |= 1 << odd_id[x]
+                    even[nums[j]] = even.get(nums[j], 0) + 1
 
-                if even_mask.bit_count() == odd_mask.bit_count():
-                    ans = max(ans, j - i + 1)
+                if len(odd) == len(even):
+                    max_len = max(max_len, j - i + 1)
 
-        return ans        # right = 1
-      
+        return max_len

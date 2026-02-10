a1aaa8ab1388bb2944be0befc20b3d712e5f8004
Sree Lekshmi Prasannan <72271005+Sree-lekshmi99@users.noreply.github.com>
2026-02-10 11:34:16 -0800

    Time: 2155 ms (7.47%) | Memory: 19.7 MB (13.62%) - LeetSync

---
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.py
@@ -0,0 +1,62 @@
+class Solution:
+    def longestBalanced(self, nums: List[int]) -> int:
+        even_id = {}
+        odd_id = {}
+        for x in nums:
+            if x % 2 == 0:
+                if x not in even_id:
+                    even_id[x] = len(even_id)
+            else:
+                if x not in odd_id:
+                    odd_id[x] = len(odd_id)
+
+        ans = 0
+        n = len(nums)
+
+        for i in range(n):
+            even_mask = 0
+            odd_mask = 0
+            for j in range(i, n):
+                x = nums[j]
+                if x % 2 == 0:
+                    even_mask |= 1 << even_id[x]
+                else:
+                    odd_mask |= 1 << odd_id[x]
+
+                if even_mask.bit_count() == odd_mask.bit_count():
+                    ans = max(ans, j - i + 1)
+
+        return ans        # right = 1
+        n = len(nums)
+        max_length = 0
+        
+
+        for i in range(len(nums)):
+            even = set()
+            odd = set()
+            if nums[i]%2==0:
+                even.add(nums[i])
+            elif nums[i]%2!=0:
+                odd.add(nums[i])
+
+        print(even)
+        print(len(even))
+        for r in range(len(nums)):
+            if len(odd) == len(even):
+                max_length = max(max_length , r-l+1)
+            else:
+                if nums[l] in odd:
+                    odd.remove(nums[l])
+                else:
+                    even.remove(nums[l])
+                l+=1
+
+        return max_length
+
+        #     if even == odd:
+        #        max_length = max(max_length(r-l+1))
+        #     elif even!= odd:
+        #         l+=1 
+        # return max_length
+
+

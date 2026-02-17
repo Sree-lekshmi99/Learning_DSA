dbf091636139be3086249cdb1ba06e6d58537cc7
Sree Lekshmi Prasannan <72271005+Sree-lekshmi99@users.noreply.github.com>
2026-02-16 17:10:44 -0800

    Time: 3 ms (3.72%) | Memory: 19.4 MB (22.30%) - LeetSync

---
+++ 290-word-pattern/word-pattern.py
@@ -0,0 +1,23 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        word = s.split()
+        map1 = {}
+        map2 = {}
+        if len(pattern) != len(word):
+            return False
+        for p, w in zip(pattern, word):
+            if p in map1 and map1[p]!=w:
+                return False
+            if w in map2 and map2[w]!=p:
+                return False
+            map1[p] = w
+            map2[w] = p
+
+            #     map1[pattern[i]] = word[i]
+            #     print(map1)
+            # elif pattern[i] in map1 or word[i] in map1:
+            #     if map1[pattern[i]] != word[i]:
+            #         return False
+        return True
+                    
+        

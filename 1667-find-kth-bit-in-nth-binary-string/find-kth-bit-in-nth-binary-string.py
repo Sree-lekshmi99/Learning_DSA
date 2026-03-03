2d452935228d9673d8a5bbb9d8d1ab477efa0fe3
Sree Lekshmi Prasannan <72271005+Sree-lekshmi99@users.noreply.github.com>
2026-03-03 15:06:29 -0800

    Time: 748 ms (6.53%) | Memory: 38.5 MB (6.10%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.py
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.py
@@ -5,7 +5,7 @@ class Solution:
         invert = ''
         s = [0]*(n+1)
         for i in range(1, n+1):
-            temp = format(i,"b")
+            # temp = format(i,"b")
             prev = str(s[i-1])
             
             invert = "".join('1' if x =='0' else '0' for x in prev)

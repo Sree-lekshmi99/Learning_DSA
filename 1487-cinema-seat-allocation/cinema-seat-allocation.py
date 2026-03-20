8e64122c19f4462bb14db984e348d56c8cff9926
Sree Lekshmi Prasannan <72271005+Sree-lekshmi99@users.noreply.github.com>
2026-03-20 15:19:29 -0700

    Time: 107 ms (13.11%) | Memory: 23.5 MB (15.19%) - LeetSync

--- 1487-cinema-seat-allocation/cinema-seat-allocation.py
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.py
@@ -4,12 +4,7 @@ class Solution:
         row_map = defaultdict(set)
         for row, col in reservedSeats:
             row_map[row].add(col)
-        print(row_map)
         ans = (n-len(row_map))*2
-
-        
-
-        # combinations = [[2345],[4567],[6789]]
 
         for row, reserved in row_map.items():
             left =  all(seat not in reserved for seat in [2,3,4,5])

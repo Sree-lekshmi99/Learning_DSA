e7b799f43b1408d0c62add8b6cab394a67041764
Sree Lekshmi Prasannan <72271005+Sree-lekshmi99@users.noreply.github.com>
2026-06-12 18:00:52 -0700

    Time: 15 ms (15.97%) | Memory: 19.3 MB (33.64%) - LeetSync

--- 4216-weighted-word-mapping/weighted-word-mapping.py
+++ 4216-weighted-word-mapping/weighted-word-mapping.py
@@ -1,21 +1,11 @@
 class Solution:
     def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
-        # l = {
-        #     a:1,b:2,c:3,d:4,e:5,f:6,g:7
-        # }
-        res = 0
+        # precompute: each letter -> its weight % 26
+        letter_weight = {chr(97 + i): weights[i] for i in range(26)}
+        
         ans = []
         for word in words:
-            res = 0
-            for i in range(len(word)):
-                position = ord(word[i]) - 97
-                res+= weights[position]
-
-            res%=26
-            ch = chr(ord('z') - res)
-            ans.append(ch)
-        return ''.join(ans)
-
+            res = sum(letter_weight[ch] for ch in word) % 26
+            ans.append(chr(ord('z') - res))
         
-
-        
+        return "".join(ans)
